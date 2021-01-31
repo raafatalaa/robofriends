b590b829eb0b12e884a927e2b0a952a83351cf73
Raafat Alaa <45883700+raafatalaa@users.noreply.github.com>
2021-01-31 18:02:09 +0200

    editing

---
+++ src/container/App.js
@@ -0,0 +1,50 @@
+import React ,{Component} from 'react'; 
+import {robots} from '../components/Robots';
+import CardsList from '../components/CardsList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import './App.css';
+
+class App extends Component{
+	constructor(){
+		super()
+		this.state = {
+			robots: [], 
+			searchfield: ''
+		}
+	}
+
+	componentDidMount(){
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => response.json())
+  		.then(users => {this.setState({robots:users})});
+	}
+
+	onSearchChanges = (event) => {
+		this.setState({searchfield : event.target.value});
+	}
+
+	
+
+	render(){
+		const filteredRobots = this.state.robots.filter(robot =>{
+			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		})
+
+		if (this.state.robots.length === 0){
+		return <h1>Loading</h1>
+		} else {
+		return (
+			<div className= "tc" >
+				<h1 className="f1">RoboFriends</h1>
+				<SearchBox onSearchChanges={this.onSearchChanges}/>
+				<Scroll>
+					<CardsList robots={filteredRobots}/>
+				</Scroll>
+			</div>
+		);
+	}
+	}
+	
+}
+export default App;
