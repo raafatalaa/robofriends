cd37caf8295764d068e16b4850ad07493b7b21be
Raafat Alaa <45883700+raafatalaa@users.noreply.github.com>
2021-02-01 18:00:41 +0200

    adding ErrorBoundry

--- src/components/CardsList.js
+++ src/components/CardsList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Card from './Card';
 
 const CardsArray =({robots}) => {
+	
    const cardComponent=robots.map((user , i)=>{
    	return <Card key ={robots[i].id} id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
    })
